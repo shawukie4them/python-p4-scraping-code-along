abdf6c520e2a1e51f5e1e850e12bb47af19a0b44
shawukie4them <shawukiekawuki@gmail.com>
2025-03-26 23:18:04 +0300

    Set defaults in course __init__

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,11 @@
 class Course:
-    pass
+    def __init__(self, title=None, schedule=None, description=None):
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+    def __str__(self):
+        output = f"Title: {self.title}\n"
+        output += f"Schedule: {self.schedule}\n"
+        output += f"Description: {self.description}\n"
+        output += "------------------"
+        return output
